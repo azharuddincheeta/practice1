452cbe9de736dd4e81005c7f49c99a9902a85162
azharuddincheeta <azharuddinncheeta@gmail.com>
2024-04-23 16:26:04 +0530

    commit from branch1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Thank you sumit!
         </a>
       </header>
     </div>
